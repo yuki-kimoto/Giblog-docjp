9c416bb9ab0ca0b05c4d970d627c2430988fb0c1
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-06-13 08:30:24 +0900

    buildのドキュメントを改善

--- templates/Giblog/Command/build.pm
+++ templates/Giblog/Command/build.pm
@@ -15,7 +15,11 @@ sub run {
 
 =head1 NAME
 
-Giblog::Command::build - build command
+Giblog::Command::build - Website building command
+
+=head1 DESCRIPTION
+
+L<Giblog::Command::build> is website building command.
 
 =head1 METHODS
 
@@ -24,6 +28,8 @@ implements the following new ones.
 
 =head2 run
 
-  $command->run(@argv);
+  $command->run;
 
-Execute build command.
+Build website.
+
+This method is planed to be overridden in subclass.
